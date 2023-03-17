bf86efb163bae7ef76a9dfc96fc30660c0474416
Tiago-soft-dev <tiago.soft.dev@gmail.com>
2023-03-17 16:26:58 -0300

    Feat: CRUD User Service

---
+++ src/services/usuario.service.js
@@ -0,0 +1,53 @@
+const Usuario = require('../model/Usuario');
+
+
+const findUserByIdService = (id) =>{
+    return Usuario.findById(id);
+
+};
+
+const findAllUsersService = () =>{
+    return Usuario.find();
+};
+
+const createUserService = (body) =>{
+    return Usuario.create(body);
+};
+
+const updateUserService = (id, body) =>{
+    return Usuario.findByIdAndUpdate(id, body {returnDocument: "after"});
+};
+
+const removeUserService = (id) =>{
+    return Usuario.findByIdAndRemove(id);
+};
+
+const addUserAddressService = (id, endereco) =>{
+
+};
+
+const removeUserAddressService = (id) => {
+    
+};
+
+const addUserFavProductUserService = (id, produto) => {
+
+};
+
+const removeUserFavProductUserService = (produto) => {
+
+};
+
+module.exports = {
+    findAllUsersService,
+    findUserByIdService,
+    createUserService,
+    updateUserService,
+    addUserAddressService,
+    removeUserService,
+    removeUserAddressService,
+    addUserFavProductUserService,
+    removeUserFavProductUserService
+
+
+}
