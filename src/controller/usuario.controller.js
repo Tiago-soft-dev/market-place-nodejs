c9523640aa06fdad5cb8fe1160d236a9d1ef276e
Tiago-soft-dev <tiago.soft.dev@gmail.com>
2023-03-17 16:07:32 -0300

    Feat: Use Controller add e rotas update

---
+++ src/controller/usuario.controller.js
@@ -0,0 +1,144 @@
+const findUserByIdController = async (req, res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+}
+
+const findAllUsersController = async (req,res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+};
+
+
+const createUserController = async (req, res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+};
+
+
+const updateUserController = async (req, res) =>{
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+};
+
+
+
+const deleteUserController = async (req, res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+    
+};
+
+const addUserAdressController = async (req, res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+};
+
+const removeUserAdressController = async (req, res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+};
+
+const addUserFavProduct = async (req, res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+};
+
+const removeUserFavProduct = async (req, res) => {
+    try{
+
+
+
+
+
+
+    }catch(err){
+                console.log(`erro: ${err.message}`)
+        return res.status(500).send({message:"Erro inexperado"});
+    }
+};
+
+
+
+module.exports = {
+    findUserByIdController,
+    findAllUsersController,
+    createUserController,
+    updateUserController,
+    deleteUserController,
+    addUserAdressController,
+    removeUserAdressController,
+    addUserFavProduct,
+    removeUserFavProduct,
+}
--- src/routes/usuario.router.js
+++ src/routes/usuario.router.js
@@ -4,19 +4,19 @@ const router = express.Router();
 
 const usuarioController=require('../controller/usuario.controller');
 
-router.get('/findById/:id');
-router.get('findAll');
+router.get('/findById/:id', usuarioController.findUserByIdController);
+router.get('findAll', usuarioController.findAllUsersController);
 
-router.post('/creste');
-router.post('/addAdress/:id');
-router.post('/addFavProduct/:id');
+router.post('/create', usuarioController.createUserController);
+router.post('/addAdress/:id', usuarioController.addUserAdressController);
+router.post('/addFavProduct/:id', usuarioController.addUserFavProduct);
 
 
-router.put('update/:id');
+router.put('update/:id', usuarioController.updateUserController);
 
-router.delete('/remove/:id');
-router.detlete('/removeAdress');
-router.delete('/removeFavProduct');
+router.delete('/remove/:id', usuarioController.deleteUserController);
+router.detlete('/removeAdress', usuarioController.removeUserAdressController);
+router.delete('/removeFavProduct', usuarioController.removeUserFavProduct);
 
 
 module.exports = router;
