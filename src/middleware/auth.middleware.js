f2701e85b41db55097fab101c7fbba28abd0e5c6
Tiago-soft-dev <tiago.soft.dev@gmail.com>
2023-03-27 11:51:29 -0300

    Feat: Autorização e middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const {findUserByIdService} = require("../services/usuario.service.js") ;
+
+module.exports = async (req,res,next) => {
+    const authHEader = req.headers.authorization;
+
+    if(!authHeader) {
+        return res.status(401).send({message: "o token nao foi informado"});
+    }
+
+    const parts = authHeader.split(" "); //["Bearer, <token>""]
+
+    if(parts.length !== 2) {
+        return res.status(401).send({message: "o token invalido"});
+
+    }
+
+    const [schema, token] = parts;
+
+    if(!/^Bearer$/i.test(schema)) {
+        return res.status(401).send({message: "o token invalido"});
+    }
+
+    jwt.verify(token, "bgfjnmgdbfhdfhmhjnfgbhgjmfjkm", async (err, decoded)=> {
+        if(err){
+            return res.status(500).send({message: "o token invalido"});
+        }
+
+        const user = await findUserByIdService(decoded.id);
+
+        if(!user || !user.id) {
+            return res.status(401).send({message: "o token invalido"});
+        }
+
+        req.userId = decoded.id;
+
+        return next();
+    })
+
+}
